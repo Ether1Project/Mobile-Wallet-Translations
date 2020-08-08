c775e44396e0c3795930be8efecf769b275d5025
FallenGravity <ethanvb55@gmail.com>
2020-08-08 15:20:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import enUS from './en-us';
+import Fr from './fr';
+import Afr from './afr';
+import Nl from './nl';
+import No from './no';
+import Sl from './sl';
+
+export default {
+  'en-us': enUS,
+  fr: Fr,
+  afr: Afr,
+  nl: Nl,
+  no: No,
+  sl: Sl,
+};
